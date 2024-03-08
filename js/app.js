a078418b8ac65456aec84ef2fcd3018a7a6438f2
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-08 19:12:18 +0100

    ADD: elementi che stampano il prezzo sul documento html quando si apre sul browser.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 //chiedere all'utente il numero di km che vuole percorrere
 
-let kmDaPercorrere =  parseFloat(prompt('Inserisci il numero di km da percorrere:')); //string | null
+let kmDaPercorrere =  parseFloat(prompt('Inserisci il numero di km da percorrere:')); //string | null \ number
 console.log("Percorrerai:" + kmDaPercorrere);
 
 //chiedere l'età del passeggero
 
-const eta = parseInt(prompt('Inserisci la tua età:')); //string | null
-console.log("La tua età è:" + eta);
+const eta = parseInt(prompt('Inserisci la tua età:')); //string | null \ number
+console.log("La tua età è:" + eta); //function
 //calcolare prezzo base
 
-const prezzoBase = kmDaPercorrere * 0.21;
-console.log("Questo è il tuo prezzo base:" + prezzoBase);
+const prezzoBase = kmDaPercorrere * 0.21; //string | null
+console.log("Questo è il tuo prezzo base:" + prezzoBase); //function
 
 //variabile prezzo finale
 
-let prezzoFinale;
+let prezzoFinale; //string | null
 
 //applicare sconto del 20% se minorenni
 
@@ -25,26 +25,27 @@ console.log("Al tuo biglietto è stato applicato lo sconto del 20%:" + prezzoFin
 //applicare sconto del 40% se over 65
 
 else if (eta > 65) {
-    prezzoFinale = prezzoBase * 0.6;
-    console.log("Al tuo biglietto è stato applicato lo sconto del 40%:" + prezzoFinale);
+    prezzoFinale = prezzoBase * 0.6;    //statement
+    console.log("Al tuo biglietto è stato applicato lo sconto del 40%:" + prezzoFinale);    //function
 }
 //calcolare il prezzo fascia eta normale
 
 else {
-    prezzoFinale = prezzoBase;
-    console.log("Al tuo biglietto è stata applicata la tariffa base:" + prezzoBase);
+    prezzoFinale = prezzoBase;  //statement
+    console.log("Al tuo biglietto è stata applicata la tariffa base:" + prezzoBase);    //function
 }
 
 //formattare il prezzo finale a due cifre decimali
 
-prezzoFinale = prezzoFinale.toFixed(2);
+prezzoFinale = prezzoFinale.toFixed(2); //string | null
 
 //stampare il prezzo finale del biglietto nella consoledel browswer in forma umana
 
-console.log("Il prezzo del bigliettoè: €" + prezzoFinale);
+console.log("Il prezzo del bigliettoè: €" + prezzoFinale);  //function
 
 //stampare il prezzo finale nella pagina html dentro a un tag p
 
- 
+const paragrafoPrezzo = document.getElementById("prezzo");  //string | null
 
+paragrafoPrezzo.innerHTML = "Prezzo del biglietto del treno: €" + prezzoFinale; //function
 
